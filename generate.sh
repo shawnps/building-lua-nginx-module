6bd81fa23d5a5e90c869320d347495c5b2bed8a0
Nick Galbreath <nickg@signalsciences.com>
2016-08-24 04:34:09 +0000

    make it so we can do per OS builds

---
+++ generate.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+OS=centos/7
+ARGS="-os=centos -from=centos:7 -maintainer signalsciences.com"
+mkdir -p ${OS} 
+cp testing/nginx.conf ${OS}
+go run main.go ${ARGS} > ${OS}/Dockerfile
+go run main.go ${ARGS} -style=docker-debug > ${OS}/Dockerfile.debug
+go run main.go ${ARGS} -style=sh > ${OS}/build-lua-ngx.sh
+chmod a+x ${OS}/build-lua-ngx.sh
+
+OS=centos/6
+ARGS="-os=centos -from=centos:6 -maintainer signalsciences.com"
+mkdir -p ${OS} 
+cp testing/nginx.conf ${OS}
+go run main.go ${ARGS} > ${OS}/Dockerfile
+go run main.go ${ARGS} -style=docker-debug > ${OS}/Dockerfile.debug
+go run main.go ${ARGS} -style=sh > ${OS}/build-lua-ngx.sh
+chmod a+x ${OS}/build-lua-ngx.sh
+
+OS=ubuntu/16.04
+ARGS="-os=ubuntu -from=ubuntu:16.04 -maintainer signalsciences.com"
+mkdir -p ${OS} 
+cp testing/nginx.conf ${OS}
+go run main.go ${ARGS} > ${OS}/Dockerfile
+go run main.go ${ARGS} -style=docker-debug > ${OS}/Dockerfile.debug
+go run main.go ${ARGS} -style=sh > ${OS}/build-lua-ngx.sh
+chmod a+x ${OS}/build-lua-ngx.sh
+
